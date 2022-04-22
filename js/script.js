2fdfd69abbb8dcc4760749ae140b2699f9983610
JeySkyet <101134762+JeySkyet@users.noreply.github.com>
2022-04-22 16:31:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.new-wallpaper__slider').owlCarousel({
+    items: 4,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText : ["<i class='arrow-left'><img src='image/arrow-left.png' /></i>","<i class='arrow-right'><img src='image/arrow-right.png' /></i>"]
+    });
+    
+    $('.colors-slider').owlCarousel({
+    items: 6,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText : ["<i class='arrow-left'><img src='image/arrow-left.png' /></i>","<i class='arrow-right'><img src='image/arrow-right.png' /></i>"]
+    });
+
+    $(".button_play").on("click", function(){
+    $('#video')[0].play();
+    $('.button_play').hide();
+    });
+    $("#video").on("click", function(){
+    $('#video')[0].pause();
+    $('.button_play').show();
+    });
+
+    $('.header__slider').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:0,
+    nav:true,
+    navText : ["<i class='arrow-left'><img src='image/arrow-left-white.png' /></i>","<i class='arrow-right'><img src='image/arrow-right-white.png' /></i>"]
+    });
+    });
